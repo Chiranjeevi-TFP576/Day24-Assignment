aaa93d11df69d59c33cd32d572e2806e2e559c93
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-11-30 19:26:08 +0530

    Solving RegexUC1-5  using Lambda

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LambdaPracticeingProblembs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            //User FirstName
+            Console.WriteLine("Enter the firstname: ");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(UserValidations.CheckFirstName(FirstName));
+            //User LastName
+            Console.WriteLine("Enter the lastname: ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine(UserValidations.CheckLastName(lastname));
+            //User Email
+            Console.WriteLine("Enter the email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine(UserValidations.checkEmail(email));
+            //User PhoneNumber
+            Console.WriteLine("Enter the phonenumber: ");
+            string phonenumber = Console.ReadLine();
+            Console.WriteLine(UserValidations.checkMobileNumber(phonenumber));
+            //User Password
+            Console.WriteLine("Enter the password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine(UserValidations.checkPassword(password));
+        }
+    }
+}
